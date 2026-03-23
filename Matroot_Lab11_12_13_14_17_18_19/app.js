a6071d17d59f2610cc3842ef5b141fece19c29cd
strberrymilk <camypowerr@gmail.com>
2026-03-23 14:42:41 -0600

    Lab21 terminado y avances Lab22

--- Matroot_Lab11_12_13_14_17_18_19/app.js
+++ Matroot_Lab11_12_13_14_17_18_19/app.js
@@ -75,3 +75,26 @@ app.use((request, response, next) => {
 
 // Inicia el servidor en el puerto 3000
 app.listen(3000);
+
+// ===========================================================================================================
+
+const multer = require('multer');
+
+//fileStorage: Es nuestra constante de configuración para manejar el almacenamiento
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'uploads': Es el directorio del servidor donde se subirán los archivos 
+        callback(null, 'uploads');
+    },
+    filename: (request, file, callback) => {
+        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+        callback(null, new Date().toISOString() + '-' + file.originalname);
+    },
+});
+
+//En el registro, pasamos la constante de configuración y
+//usamos single porque es un sólo archivo el que vamos a subir, 
+//pero hay diferentes opciones si se quieren subir varios archivos. 
+//'archivo' es el nombre del input tipo file de la forma
+app.use(multer({ storage: fileStorage }).single('archivo')); 
